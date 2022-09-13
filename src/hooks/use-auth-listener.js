2ac37cbc2a7468403e451b50b57e363c5885213a
Benedick Sahagun <benedicksahagun@gmail.com>
2022-09-14 01:25:46 +0800

    update: useAuth hook deps

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,25 +1,24 @@
-import { useState, useEffect, useContext } from 'react';
-import { FirebaseContext } from '../context/firebase';
+import { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
 
 export default function useAuthListener() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("authUser")));
 
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
     const listener = firebase.auth().onAuthStateChanged((authUser) => {
       if (authUser) {
-        localStorage.setItem('authUser', JSON.stringify(authUser));
+        localStorage.setItem("authUser", JSON.stringify(authUser));
         setUser(authUser);
       } else {
-        localStorage.removeItem('authUser');
+        localStorage.removeItem("authUser");
         setUser(null);
       }
     });
 
     return () => listener();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [setUser, firebase]);
 
   return { user };
 }
